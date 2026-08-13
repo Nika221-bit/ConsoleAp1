1b1db4b53c3d46675ad31ec9a9b58e725e528c0b
niko5443 <184934855+Nika221-bit@users.noreply.github.com>
2026-08-13 21:10:18 +0400

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,1 @@
+﻿using ConsoleApp1.moduls;
---
+++ ConsoleApp1/moduls/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1.moduls;
+
+public class Student
+{
+    
+    public string Name { get; set; }
+    public string Lastname { get; set; }
+    public int Age { get; set; }
+    public string Email { get; set; }
+    
+    public Student(string name, string lastname, int age, string email)
+    {
+        Name = name;
+        Lastname = lastname;
+        Age = age;
+        Email = email;
+    }
+    
+}
+
